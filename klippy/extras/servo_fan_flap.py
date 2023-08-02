7bb2cf4ebf50eec1ade9c6e271882a8aa3f80b0f
Michal Tomek <tomekmichalhu@seznam.cz>
2023-08-02 11:25:24 +0200

    Servo set width command, autotune command, springback parameter

--- klippy/extras/servo_fan_flap.py
+++ klippy/extras/servo_fan_flap.py
@@ -19,8 +19,11 @@ class SERVO_STATE_MACHINE(Enum):
 
 class ServoFanFlap:
     cmd_FLAP_DEBUG_help = "Returns debug informations."
-    cmd_FLAP_SET_help = "Sets the flap position. Usage: FLAP_SET flap_name " \
-                        "VALUE=<0. - 1. | 0 - 255>"
+    cmd_FLAP_SET_help = "Sets the flap position. Usage: FLAP_SET FLAP=flap_name " \
+                        "[ VALUE=<0. - 1. | 0 - 255> | WIDTH=pulse_width ]"
+    cmd_FLAP_AUTOTUNE_help = "Finds flap range. Usage: FLAP_AUTOTUNE FLAP=flap_name " \
+                             "MIN_PW=min_pulse_width MAX_PW=max_pulse_width START_PW=start_pulse_width " \
+                             "SPRINGBACK=spring_back"
     def __init__(self, config):
         self.printer = config.get_printer()
         self.servo = servo.PrinterServo(config)
@@ -32,10 +35,11 @@ class ServoFanFlap:
 
         self.is_print_fan = config.getboolean("is_print_fan", False)
         self.open_at_sp = config.getboolean("open_at_sp", False)
-        self.min_pulse_width = config.getfloat("minimum_pulse_width", 0.001)
-        self.max_pulse_width = config.getfloat("maximum_pulse_width", 0.002)
+        self.min_pulse_width = self.tuning_min_pulse_width = config.getfloat("minimum_pulse_width", 0.001)
+        self.max_pulse_width = self.tuning_max_pulse_width = config.getfloat("maximum_pulse_width", 0.002)
         self.tuning_start_width = config.getfloat("tuning_start_width", self.min_pulse_width + (
                 self.max_pulse_width - self.min_pulse_width) * 0.5)
+        self.tuning_spring_back = config.getfloat("tuning_spring_back", 0.0001)
         self.start_value = config.getfloat(
             "start_value", 0, minval=0, maxval=1)
         self.last_value = -1
@@ -79,6 +83,10 @@ class ServoFanFlap:
                                    self.flap_name,
                                    self.cmd_FLAP_SET,
                                    desc=self.cmd_FLAP_SET_help)
+        gcode.register_mux_command("FLAP_AUTOTUNE", "FLAP",
+                                   self.flap_name,
+                                   self.cmd_FLAP_AUTOTUNE,
+                                   desc=self.cmd_FLAP_AUTOTUNE_help)
         if self.is_print_fan:
             gcode.register_command("M106", self.cmd_M106)
             gcode.register_command("M107", self.cmd_M107)
@@ -88,10 +96,44 @@ class ServoFanFlap:
                           f"width: {self.min_pulse_width}, max pulse width: "
                           f"{self.max_pulse_width}, servo state machine: {self.tuning_state}")
     def cmd_FLAP_SET(self, gcmd):
-        val = gcmd.get_float('VALUE', minval=0., maxval= 255.)
-        if val > 1:
-            val = val / 255
-        self.set_value(val)
+        width = gcmd.get_float('WIDTH', None)
+        if width is not None:
+            self.servo.set_width(width)
+        else:
+            val = gcmd.get_float('VALUE', minval=0., maxval= 255.)
+            if val > 1:
+                val = val / 255
+            self.set_value(val)
+
+    def cmd_FLAP_AUTOTUNE(self, gcmd):
+        width_changed = False
+        tuning_min_pulse_width = gcmd.get_float("MIN_PW", None)
+        if tuning_min_pulse_width is not None:
+            width_changed = True
+            self.min_pulse_width = self.tuning_min_pulse_width = tuning_min_pulse_width
+        else:
+            self.min_pulse_width = self.tuning_min_pulse_width
+
+        tuning_max_pulse_width = gcmd.get_float("MAX_PW", None)
+        if tuning_max_pulse_width is not None:
+            width_changed = True
+            self.max_pulse_width = self.tuning_max_pulse_width = tuning_max_pulse_width
+        else:
+            self.max_pulse_width = self.tuning_max_pulse_width
+
+        tuning_start_width = gcmd.get_float("START_PW", None)
+        if width_changed and tuning_start_width is None:
+            tuning_start_width = self.min_pulse_width + (self.max_pulse_width - self.min_pulse_width) * 0.5
+
+        if tuning_start_width is not None:
+            self.tuning_start_width = tuning_start_width
+
+        spring_back = gcmd.get_float("SPRINGBACK", None)
+        if spring_back is not None:
+            self.tuning_spring_back = spring_back
+
+        self.actual_tuning_width = self.tuning_start_width
+        self.tuning_state = SERVO_STATE_MACHINE.TUNING_START
 
     def cmd_M106(self, gcmd):
         val = gcmd.get_float('S', 255., minval=0.) / 255.
@@ -99,7 +141,7 @@ class ServoFanFlap:
     def cmd_M107(self, gcmd):
         self.set_value(0)
 
-    def set_value(self, value):
+    def set_value(self, value, print_time=None):
         if math.isclose(self.last_value,value):
             return
         self.last_value = value
@@ -110,7 +152,7 @@ class ServoFanFlap:
         eventtime = self.reactor.monotonic()
         self.power_off_timeout = eventtime + self.power_off_time
         self.is_on = True
-        self.servo.set_width(w)
+        self.servo.set_width(w, print_time)
 
     def _analog_feedback_callback(self, last_read_time, last_value):
         # This callback is called periodically. I need it for servo range
@@ -137,7 +179,7 @@ class ServoFanFlap:
         print_time = self.mcu.estimated_print_time(eventtime)
         if self.tuning_state == SERVO_STATE_MACHINE.TUNING_START:
             self.power_off_timeout = eventtime + self.power_off_time
-            self.servo.set_width(self.actual_tuning_width)
+            self.servo.set_width(self.actual_tuning_width, print_time)
             self.tuning_timeout = print_time + self.tuning_start_time
             self.tuning_state = SERVO_STATE_MACHINE.TUNING_PHASE_1
             return
@@ -146,9 +188,8 @@ class ServoFanFlap:
             if last_read_time < self.tuning_timeout:
                 return
             if last_value > self.tuning_treshold:
-                max_w = self.actual_tuning_width
+                self.max_pulse_width = self.actual_tuning_width - self.tuning_spring_back
                 self.actual_tuning_width = self.tuning_start_width
-                self.max_pulse_width = max_w
                 self.tuning_state = SERVO_STATE_MACHINE.TUNING_PHASE_2
                 self.tuning_timeout = print_time + self.tuning_start_time
             else:
@@ -160,16 +201,16 @@ class ServoFanFlap:
                 else:
                     self.tuning_timeout = print_time + self.tuning_step_time
             self.power_off_timeout = eventtime + self.power_off_time
-            self.servo.set_width(self.actual_tuning_width)
+            self.servo.set_width(self.actual_tuning_width, print_time)
             return
 
         if self.tuning_state == SERVO_STATE_MACHINE.TUNING_PHASE_2:
             if last_read_time < self.tuning_timeout:
                 return
             if last_value > self.tuning_treshold:
-                self.min_pulse_width = self.actual_tuning_width
+                self.min_pulse_width = self.actual_tuning_width + self.tuning_spring_back
                 self.tuning_state = SERVO_STATE_MACHINE.TUNED
-                self.set_value(self.start_value)
+                self.set_value(self.start_value, print_time)
             else:
                 self.actual_tuning_width -= self.tuning_step
                 if self.actual_tuning_width < self.min_pulse_width:
@@ -177,10 +218,10 @@ class ServoFanFlap:
                 else:
                     self.tuning_timeout = print_time + self.tuning_step_time
                     self.power_off_timeout = eventtime + self.power_off_time
-                    self.servo.set_width(self.actual_tuning_width)
+                    self.servo.set_width(self.actual_tuning_width, print_time)
             return
         if self.tuning_state == SERVO_STATE_MACHINE.TUNED:
-            self.set_value(self.start_value)
+            self.set_value(self.start_value, print_time)
             self.tuning_state = SERVO_STATE_MACHINE.ALL_DONE
             return
 def load_config_prefix(config):
