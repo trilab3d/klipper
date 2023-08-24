90ee84771f967e6080307e6eca6823d5a2e6ac44
Matej Supik <supik.matej@seznam.cz>
2023-08-24 08:57:31 +0200

    Fix stepper flap

--- klippy/extras/stepper_flap.py
+++ klippy/extras/stepper_flap.py
@@ -9,6 +9,8 @@ class SERVO_STATE_MACHINE(Enum):
     DISABLING = 2
     IDLE = 3
 
+FLUSH_DELAY = 0.001
+
 class StepperFlap:
     cmd_FLAP_SET_help = "Sets the flap position. Usage: FLAP_SET FLAP=flap_name " \
                         "[ VALUE=<0. - 1. | 0 - 255> | WIDTH=pulse_width ]"
@@ -40,7 +42,8 @@ class StepperFlap:
         self.stepper.setup_itersolve('cartesian_stepper_alloc', b'x')
         self.stepper.set_trapq(self.trapq)
 
-        self.is_print_fan = config.getboolean("is_print_fan", False)
+        self.invert = config.getboolean('invert', False)
+
         self.wanted_value = config.getfloat("start_value", 0, minval=0, maxval=1)
 
         # register commands
@@ -49,37 +52,44 @@ class StepperFlap:
                                    self.flap_name,
                                    self.cmd_FLAP_SET,
                                    desc=self.cmd_FLAP_SET_help)
-        if self.is_print_fan:
-            gcode.register_command("M106", self.cmd_M106)
-            gcode.register_command("M107", self.cmd_M107)
 
     def _handle_connect(self):
         self.toolhead = self.printer.lookup_object('toolhead')
 
-        self.update_timer.waketime = self.reactor.monotonic() + 2
+        self.update_timer.waketime = self.reactor.monotonic() + 1
+        self.disable_timer.waketime = self.reactor.monotonic() + 2            
 
     def cmd_FLAP_SET(self, gcmd):
-        width = gcmd.get_float('WIDTH', None)
-        if width is not None:
-            return
-        else:
-            val = gcmd.get_float('VALUE', minval=0., maxval= 255.)
-            if val > 1:
-                val = val / 255
-            self.requested_value = val
+        val = gcmd.get_float('VALUE', minval=0., maxval= 255.)
+        if val > 1:
+            val = val / 255
+        self.set_value(val)
 
-    def cmd_M106(self, gcmd):
-        self.requested_value = gcmd.get_float('S', 255., minval=0.) / 255.
-        
-    def cmd_M107(self, gcmd):
-        self.requested_value = 0
+    def set_value(self, value):
+        if self.invert:
+            self.requested_value = -1.0 * value
+        else:
+            self.requested_value = value
+
+        eventtime = self.reactor.monotonic()
+        toolhead = self.printer.lookup_object('toolhead')
+        _, _, lookahead_empty = toolhead.check_busy(
+            eventtime)
+
+        if lookahead_empty:
+            self.update_timer.waketime = self.reactor.monotonic() + 0.5
+            self.do_move(self.requested_value, self.velocity, self.accel, True)
 
     def do_update_value(self, time):
         move_time = 0.05
         if self.current_value != self.requested_value:
-            move_time = self.set_value(self.requested_value)
+            move_time = self.do_move(self.requested_value, self.velocity, self.accel, False)
 
-        self.current_value = self.requested_value
+            print_time = self.printer.lookup_object('toolhead').print_time
+            if self.next_cmd_time > print_time:
+                move_time += (self.next_cmd_time - print_time)
+
+            self.current_value = self.requested_value
 
         return self.reactor.monotonic() + move_time
     
@@ -89,25 +99,20 @@ class StepperFlap:
         self.toolhead.register_lookahead_callback((lambda pt: se.motor_disable(pt)))
         return self.reactor.NEVER
 
-    def set_value(self, value):
-        self.disable_timer.waketime = self.reactor.NEVER
-        move_time = self.do_move(value, self.velocity, self.accel)
-        self.disable_timer.waketime = self.reactor.monotonic() + 1
-
-        return move_time
-
     def sync_print_time(self):
-        curtime = self.reactor.monotonic()
-        est_print_time = self.toolhead.mcu.estimated_print_time(curtime)
         print_time = self.toolhead.get_last_move_time()
-        print_time = max(print_time, est_print_time)
         if self.next_cmd_time > print_time:
             self.toolhead.dwell(self.next_cmd_time - print_time)
         else:
             self.next_cmd_time = print_time
 
-    def do_move(self, movepos, speed, accel):
-        self.sync_print_time()
+    def do_move(self, movepos, speed, accel, sync=True):
+        self.disable_timer.waketime = self.reactor.NEVER
+        if sync:
+            self.sync_print_time()
+        else:
+            self.next_cmd_time = self.printer.lookup_object('toolhead').print_time
+
         cp = self.stepper.get_commanded_position()
         dist = movepos - cp
         axis_r, accel_t, cruise_t, cruise_v = force_move.calc_move_time(
@@ -118,9 +123,12 @@ class StepperFlap:
                           0., cruise_v, accel)
         self.next_cmd_time = self.next_cmd_time + accel_t + cruise_t + accel_t
         self.stepper.generate_steps(self.next_cmd_time)
-        self.trapq_finalize_moves(self.trapq, self.next_cmd_time + 99999.9)
+        self.trapq_finalize_moves(self.trapq, self.next_cmd_time + 2.)
         toolhead = self.printer.lookup_object('toolhead')
         toolhead.note_kinematic_activity(self.next_cmd_time)
+        if sync:
+            self.sync_print_time()
+        self.disable_timer.waketime = self.reactor.monotonic() + 2
         return accel_t + cruise_t + accel_t
 
 def load_config_prefix(config):
