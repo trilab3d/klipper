337eb4cdd2cad07c4db1840ad0c827e7d60d7542
Michal Tomek <tomekmichalhu@seznam.cz>
2025-03-05 11:57:41 +0100

    Report time remaining to next pause; progress reset on print start to prevent start with 100% displayed

--- klippy/extras/display_status.py
+++ klippy/extras/display_status.py
@@ -10,7 +10,9 @@ M73_TIMEOUT = 5.
 class DisplayStatus:
     def __init__(self, config):
         self.printer = config.get_printer()
-        self.progress = self.message = self.remaining = None
+        self.printer.register_event_handler("virtual_sdcard:reset_file",
+                                            self._reset_file)
+        self.progress = self.message = self.remaining = self.remaining_to_pause = None
         # Register commands
         gcode = self.printer.lookup_object('gcode')
         gcode.register_command('M73', self.cmd_M73)
@@ -25,21 +27,32 @@ class DisplayStatus:
             sdcard = self.printer.lookup_object('virtual_sdcard', None)
             if sdcard is not None:
                 progress = sdcard.get_status(eventtime)['progress']
-        return { 'progress': progress, 'message': self.message, 'remaining': self.remaining }
+        return {
+            'progress': progress,
+            'message': self.message,
+            'remaining': self.remaining,
+            'remaining_to_pause': self.remaining_to_pause
+        }
     def cmd_M73(self, gcmd):
         progress = gcmd.get_float('P', None)
         remaining = gcmd.get_float('R', None)
+        remaining_to_pause = gcmd.get_float('C', None)
         if progress is not None:
             progress = progress / 100.
             self.progress = min(1., max(0., progress))
         if remaining is not None:
             self.remaining = remaining
+        if remaining_to_pause is not None:
+            self.remaining_to_pause = remaining_to_pause
     def cmd_M117(self, gcmd):
         msg = gcmd.get_raw_command_parameters() or None
         self.message = msg
     cmd_SET_DISPLAY_TEXT_help = "Set or clear the display message"
     def cmd_SET_DISPLAY_TEXT(self, gcmd):
         self.message = gcmd.get("MSG", None)
+
+    def _reset_file(self):
+        self.progress = self.message = self.remaining = self.remaining_to_pause = None
 
 def load_config(config):
     return DisplayStatus(config)
